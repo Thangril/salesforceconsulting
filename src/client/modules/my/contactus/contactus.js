dad399a9d13e39903de05ee8e5b7332618f17478
Thangril <50687122+Thangril@users.noreply.github.com>
2022-05-10 10:38:50 +0900

    1

--- src/client/modules/my/contactus/contactus.js
+++ src/client/modules/my/contactus/contactus.js
@@ -1,6 +1,16 @@
 import {LightningElement} from 'lwc'
 import CONTACT_US from '../../../data/contactusData'
 export default class Contactus extends LightningElement{
-    
+    formData={}
+
     contactUs = CONTACT_US
+
+    formHandler(event){
+        const {name, value} = event.target
+        this.formData[name] = value
+      
+    }
+    sendEmail(){
+        console.log(this.formData)
+    }
 }
